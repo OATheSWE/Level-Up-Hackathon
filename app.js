86f137294052e157294974cd8f755e70d440add1
OATheSWE <osborneosas12@gmail.com>
2023-11-25 05:07:59 +0100

    Finished merging second component & doing all the touchups

--- app.js
+++ app.js
@@ -4,39 +4,62 @@
 const dropdowns = document.querySelectorAll(".drp");
 const dropdownsBtn = document.querySelectorAll(".drp-btn");
 
+// Variable to keep track of the currently open dropdown
+let activeDropdown = null;
+
 // Function to toggle the dropdown animation for a specific index
 function toggleDropdownAnimation(index) {
-    dropdowns[index].classList.toggle("drp-anim");
-}
+  if (activeDropdown !== null && activeDropdown !== dropdowns[index]) {
+    // Close the currently open dropdown if it's not the one being toggled
+    activeDropdown.classList.remove("drp-anim");
+  }
 
-// Function to close all dropdowns
-function closeAllDropdowns() {
-    // Iterate through all dropdowns and remove the "drp-anim" class
-    for (let j = 0; j < dropdowns.length; j++) {
-        dropdowns[j].classList.remove("drp-anim");
-    }
+  // Toggle the animation class for the clicked button
+  dropdowns[index].classList.toggle("drp-anim");
+  activeDropdown = dropdowns[index].classList.contains("drp-anim") ? dropdowns[index] : null;
 }
 
 // Event listeners for dropdown buttons
 for (let i = 0; i < dropdownsBtn.length; i++) {
-    dropdownsBtn[i].addEventListener("click", function (event) {
-        event.stopPropagation(); // Prevents the click event from reaching the document
-        toggleDropdownAnimation(i); // Toggle the dropdown animation for the clicked button
-    });
+  dropdownsBtn[i].addEventListener("click", function (event) {
+    event.stopPropagation(); // Prevents the click event from reaching the document
+    toggleDropdownAnimation(i); // Toggle the dropdown animation for the clicked button
+  });
+  dropdownsBtn[i].addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault(); // Prevents the default behavior (e.g., scrolling)
+      toggleDropdownAnimation(i); // Toggle the dropdown animation for the clicked button
+    }
+  });
 }
 
-// Event listener added to the document to close all dropdowns when clicking outside of them
+// Event listener added to the document to close the dropdown when clicking outside of it
 document.addEventListener("click", function () {
-    closeAllDropdowns();
+  if (activeDropdown !== null) {
+    activeDropdown.classList.remove("drp-anim");
+    activeDropdown = null;
+  }
 });
 
-// Additional event listeners to prevent propagation for dropdowns themselves
-for (let j = 0; j < dropdowns.length; j++) {
-    dropdowns[j].addEventListener("click", function (event) {
-        event.stopPropagation(); // Prevents the click event from reaching the document
-    });
-}
 
 
 
 
+
+// CODE TO HIDE BOX FROM THE PAGE
+
+// Get reference to the cancel icon and the box
+const cancelIcon = document.getElementById("cancelIcon");
+const box = document.getElementById("myBox");
+
+
+// Event Listeners for the cancel icon
+cancelIcon.addEventListener("click", function () {
+  box.classList.add("box-anim");
+});
+cancelIcon.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" || event.key === " ") {
+    // Trigger the button click or perform the desired action
+    box.classList.add("box-anim");
+  }
+});
